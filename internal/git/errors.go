563d81dedcfc8d331ddd58cbf15b901eee413c44
Juho <134906793+orot11955@users.noreply.github.com>
2026-08-31 14:36:04 +0900

    fix: command argument secret stderr 재노출 차단

--- internal/git/errors.go
+++ internal/git/errors.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os/exec"
 	"slices"
+	"strings"
 )
 
 var ErrConfigNotSet = errors.New("repository config is not set")
@@ -27,12 +28,36 @@ func commandFailure(args []string, stderr string, cause error) error {
 	if message == "" && cause != nil {
 		message = redactGitError(cause.Error())
 	}
+	message = redactArgumentSecrets(message, args)
 	code := -1
 	var exitErr *exec.ExitError
 	if errors.As(cause, &exitErr) {
 		code = exitErr.ExitCode()
 	}
 	return &CommandError{Args: slices.Clone(args), Message: message, ExitCode: code, cause: cause}
+}
+
+func redactArgumentSecrets(message string, args []string) string {
+	for _, arg := range args {
+		for _, match := range gitQuerySecretPattern.FindAllString(arg, -1) {
+			if _, secret, ok := strings.Cut(match, "="); ok && secret != "" {
+				message = strings.ReplaceAll(message, secret, "<redacted>")
+			}
+		}
+		if credentialURL := gitURLUserPattern.FindString(arg); credentialURL != "" {
+			parts := strings.SplitN(credentialURL, "://", 2)
+			if len(parts) == 2 {
+				userinfo := strings.TrimSuffix(parts[1], "@")
+				if userinfo != "" {
+					message = strings.ReplaceAll(message, userinfo, "<redacted>")
+					if _, password, ok := strings.Cut(userinfo, ":"); ok && password != "" {
+						message = strings.ReplaceAll(message, password, "<redacted>")
+					}
+				}
+			}
+		}
+	}
+	return message
 }
 
 func IsExitCode(err error, code int) bool {
